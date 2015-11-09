df94f1844432ef4569f1ee349bf207339a89ac44
Cicciolina Miranda <cicciolina.miranda@cloudsherpas>
2015-11-09 17:54:16 +0800

    Remove unnecessary and duplicate endpoints and services and fix getting rules config path on RulesConfig

--- drools-demo-app/src/main/java/com/cloudsherpas/droolsample/config/RulesConfig.java
+++ drools-demo-app/src/main/java/com/cloudsherpas/droolsample/config/RulesConfig.java
@@ -1,8 +1,8 @@
 package com.cloudsherpas.droolsample.config;
 
-import java.io.IOException;
-import java.io.InputStream;
-
+import com.cloudsherpas.droolsample.config.property.RulesProperties;
+import com.cloudsherpas.droolsample.domain.RuleArtifact;
+import com.cloudsherpas.droolsample.repository.RuleArtifactRepository;
 import org.drools.core.io.impl.UrlResource;
 import org.kie.api.KieServices;
 import org.kie.api.builder.KieModule;
@@ -13,7 +13,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import com.cloudsherpas.droolsample.config.property.RulesProperties;
+import java.io.IOException;
+import java.io.InputStream;
+
+import static com.cloudsherpas.droolsample.util.RuleUtil.createPath;
 
 /**
  * @author RMPader
@@ -24,36 +27,35 @@ public class RulesConfig {
     @Autowired
     private RulesProperties rulesProperties;
 
+    @Autowired
+    private RuleArtifactRepository ruleArtifactRepository;
+
     @Bean
     public KieContainer kieContainer() throws IOException {
         KieServices ks = KieServices.Factory.get();
         KieRepository repo = ks.getRepository();
         KieResources resources = ks.getResources();
 
-        // TODO: make this configurable at runtime. Include in M2 (Milestone 2).
-//        String userpassword = "admin:admin";
-        String url = rulesProperties.getRulesRepoPath()
-                + "com/cloudsherpas/rule/1.0/rule-1.0.jar";
+        RuleArtifact ruleArtifact = ruleArtifactRepository.findByActiveTrue();
 
-        // HttpURLConnection httpURLConnection = (HttpURLConnection)new
-        // URL(url).openConnection();
-        // String authEnc = new Base64Encoder().encode(userpassword.getBytes());
-        // httpURLConnection.setRequestProperty("Authorization", "Basic " +
-        // authEnc);
-        // repo.addKieModule(resources.newInputStreamResource(
-        // httpURLConnection.getInputStream()));
-        // // ReleaseIdImpl releaseId = new ReleaseIdImpl("demo",
-        // "CourseSuggestion", "LATEST");
-        // ReleaseIdImpl releaseId = new ReleaseIdImpl("com.cloudsherpas",
-        // "rule", "LATEST");
+        String url;
+        if (ruleArtifact == null) {
+            url = rulesProperties.getRulesRepoPath() + "com/cloudsherpas/rule/1.0/rule-1.0.jar";
+        } else {
+            url = rulesProperties.getRulesRepoPath()
+                    + createPath(ruleArtifact.getGroupId(),
+                                 ruleArtifact.getArtifactId(),
+                                 ruleArtifact.getVersion());
+        }
+
         UrlResource urlResource = (UrlResource) ks.getResources()
-                .newUrlResource(url);
+                                                  .newUrlResource(url);
         urlResource.setUsername(rulesProperties.getUsername());
         urlResource.setPassword(rulesProperties.getPassword());
         urlResource.setBasicAuthentication("enabled");
         InputStream is = urlResource.getInputStream();
         KieModule k = repo.addKieModule(resources.newInputStreamResource(is));
+
         return ks.newKieContainer(k.getReleaseId());
     }
-
 }
