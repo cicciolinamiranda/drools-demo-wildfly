a82152fe170f47983c1709129fd9a7545d345fd0
Reynald Pader <reynaldmpader@gmail.com>
2015-11-26 15:39:48 +0800

    Fix error handling in CourseService.adviceCourses()

--- drools-demo-app/src/main/java/com/cloudsherpas/droolsample/service/CourseService.java
+++ drools-demo-app/src/main/java/com/cloudsherpas/droolsample/service/CourseService.java
@@ -1,9 +1,12 @@
 package com.cloudsherpas.droolsample.service;
 
+import com.cloudsherpas.droolsample.api.exception.InvalidParameterException;
 import com.cloudsherpas.droolsample.dto.CourseListDTO;
 import com.cloudsherpas.droolsample.model.StudentSubjectRating;
 import org.kie.api.runtime.KieContainer;
 import org.kie.api.runtime.StatelessKieSession;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -18,28 +21,31 @@ import java.util.stream.Collectors;
 @Service
 public class CourseService {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(CourseService.class);
+
     @Autowired
     private KieContainer kieContainer;
 
     public CourseListDTO adviceCourses(Map<String, String> subjectRatingMap) {
-        Map<String, Integer> ratings = subjectRatingMap.entrySet()
-                                                       .stream()
-                                                       .collect(Collectors.toMap(Map.Entry::getKey,
-                                                                                 e -> Integer.valueOf(
-                                                                                         e.getValue())));
-        CourseListDTO courseListDTO = new CourseListDTO();
-        StudentSubjectRating studentSubjectRating = new StudentSubjectRating(ratings);
         try {
+            Map<String, Integer> ratings = subjectRatingMap.entrySet()
+                                                           .stream()
+                                                           .collect(Collectors.toMap(Map.Entry::getKey,
+                                                                                     e -> Integer.valueOf(
+                                                                                             e.getValue())));
+            CourseListDTO courseListDTO = new CourseListDTO();
+            StudentSubjectRating studentSubjectRating = new StudentSubjectRating(ratings);
             StatelessKieSession courseMatchSession = kieContainer
                     .newStatelessKieSession();
             List<StudentSubjectRating> facts = new ArrayList<>();
             facts.add(studentSubjectRating);
             courseMatchSession.setGlobal("courseListDTO", courseListDTO);
             courseMatchSession.execute(facts);
+            return courseListDTO;
         } catch (Exception ex) {
-            ex.printStackTrace();
+            LOGGER.error("Encountered error while processing course ratings", ex);
+            throw new InvalidParameterException();
         }
-        return courseListDTO;
     }
 
 }
